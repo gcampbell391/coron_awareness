7d7f688946832ce5410295c8829601c5434808f8
gcampbell391 <gcampbell391@gmail.com>
2020-10-29 13:16:05 -0400

    more updates

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import CoronaMap from '../components/CoronaMap'
+import Blink from 'react-blink-text';
+
 
 
 const Home = () => {
 
     const [chartData, setChartData] = useState([['Country', 'New Confirmed Cases']])
     const [updateDate, setUpdateDate] = useState('')
+    const [globalData, setGlobalData] = useState('')
 
     useEffect(() => {
         fetch('https://api.covid19api.com/summary')
             .then(resp => resp.json())
             .then(data => {
+                setGlobalData(data.Global)
                 setUpdateDate(`${data.Date.split('-')[1]}/${data.Date.split('-')[2].split('T')[0]}/${data.Date.split('-')[0]}`)
                 data.Countries.forEach(country => {
                     console.log(country)
@@ -22,8 +26,10 @@ const Home = () => {
     return (
         <div className='home'>
             <h1 id='home-title'>CORON-AWARENESS</h1>
-            <p id='home-update-title'>Last Updated: {updateDate}</p>
+            <p id='home-total-new-title'>Total New Cases Confirmed: </p>
+            <p id='home-total-new-title-numbers'><Blink color='red' text={globalData.NewConfirmed} fontSize='35'>Total Cases</Blink></p>
             <CoronaMap data={chartData} />
+            <p id='home-update-title'>Last Updated: {updateDate}</p>
         </div>
     )
 }
